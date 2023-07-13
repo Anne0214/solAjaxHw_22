1be6fe3754f481f7cb87055bf7204e47a50e3876
Anne0214 <63950140+Anne0214@users.noreply.github.com>
2023-07-13 21:59:15 +0800

    feat : ajax作業1與作業2

---
+++ prjAjaxHw/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prjAjaxHw.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
